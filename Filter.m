2dc44ed4670932ac38e10097e44f47b6b75b1506
Charles, Kim <mckim2013@gmail.com>
2021-01-04 22:57:00 +0900

    Update Filter.m

--- Filter.m
+++ Filter.m
@@ -1,16 +1,15 @@
-% Nfft = 128;
-% [h, f] = freqz(data, 1, Nfft, fs);
-% semilogx(f,mag2db(abs(h)))
+% This function works as a filter that convolutes impulse reponse of HRTF(KEMAR) and input signal 
 
 % Read song data from wav file 
-filename = 'Cello1.wav';
-[y, Fs] = audioread(filename);
+filename = 'InstrumentSound.wav';
+[y, Fs] = audioread(filename); % Note that Fs here, fsL, and fsR has to be identical for precise convolution 
 
-% Stereo 
+% Stereo: y is n by 2 double type data
 yL = y(:, 1);
 yR = y(:, 2);
 
 % Read impulse response of given direction HRTF
+% wav files from MIT KEMAR research, 1994
 Left = 'L30e042a.wav';
 Right = 'R30e042a.wav';
 
@@ -25,10 +24,14 @@ y = [yL yR];
 % data to wav file 
 filename = 'RightCello1.wav';
 audiowrite(filename, y, Fs);
-% clear(y, Fs);
-% sound(y, Fs);
+clear(y, Fs); % Clear method may not operate well for stereo data, 512 by 2 double datatype for instance
+sound(y, Fs);
 
 
+% In case of FFT needed in order to convert into frequency domain
+% Nfft = 128;
+% [h, f] = freqz(data, 1, Nfft, fs);
+% semilogx(f,mag2db(abs(h)))
 
 
 
